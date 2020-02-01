f5de6c904a824a3ba48c858d2579b4af2b03db3b
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-02-01 15:49:32 +0000

    B4 Mapbox ng

--- App/src/environments/environment.ts
+++ App/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoianVzdGdpbmdlcjg4OCIsImEiOiJjazYzcGh1dGwwODFpM25tcHpxcXE2dDB5In0.CTmAALwYJmMeAtBJL-3-xA'
+  }
 };
 
 /*
