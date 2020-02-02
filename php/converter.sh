bbe3fcef52deab49567c97955758ddb3f4e94017
James Whale <43613409+WhaleJ84@users.noreply.github.com>
2020-02-02 01:58:48 +0000

    Add files via upload

--- php/converter.sh
+++ php/converter.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 latitude=$(php json_generator.php 2>/dev/null | cut -f1 -d,)
-longitute=$(php json_generator.php 2>/dev/null | cut -f2 -d,)
+longitude=$(php json_generator.php 2>/dev/null | cut -f2 -d,)
 
-sed -E "s/\$lat/$latitude/; s/\$long/$longitude/" example.json > generated.json
+echo "$latitude,$longitude"
+sed -E "s/\$lat/$latitude/; s/\$lon/$longitude/" example.json > generated.json
--- php/json_generator.php
+++ php/json_generator.php
@@ -1,21 +1,32 @@
 #!/usr/bin/php
 <?php
 
-$united_kingdom = ["north_digit" => "58",
-                   "north_decimal" => "6350001085",
-                   "east_digit" => "1",
-                   "east_decimal" => "68153079591",
-                   "south_digit" => "49",
-                   "south_decimal" => "959999905",
-                   "west_digit" => "-7",
-                   "west_decimal" => "57216793459"];
+function uk_geo_generator() {
+    $united_kingdom = ["north_digit" => "58",
+        "north_decimal" => "6350001085",
+        "east_digit" => "1",
+        "east_decimal" => "68153079591",
+        "south_digit" => "49",
+        "south_decimal" => "959999905",
+        "west_digit" => "-7",
+        "west_decimal" => "57216793459"];
 
-$random_latitude_digit = (mt_rand($united_kingdom['south_digit'],$united_kingdom['north_digit']));
-$random_latitude_decimal = (mt_rand($united_kingdom['south_decimal'],$united_kingdom['north_decimal']));
-$random_longitude_digit = (mt_rand($united_kingdom['west_digit'],$united_kingdom['east_digit']));
-$random_longitude_decimal = (mt_rand($united_kingdom['west_decimal'],$united_kingdom['east_decimal']));
-$random_location = ($random_latitude_digit . "." . $random_latitude_decimal . "," . $random_longitude_digit . "." . $random_longitude_decimal);
+    $random_latitude_digit = (mt_rand($united_kingdom['south_digit'],$united_kingdom['north_digit']));
+    $random_latitude_decimal = (mt_rand($united_kingdom['south_decimal'],$united_kingdom['north_decimal']));
+    $random_latitude = ($random_latitude_digit . "." . $random_latitude_decimal);
+    $random_longitude_digit = (mt_rand($united_kingdom['west_digit'],$united_kingdom['east_digit']));
+    $random_longitude_decimal = (mt_rand($united_kingdom['west_decimal'],$united_kingdom['east_decimal']));
+    $random_longitude = ($random_longitude_digit . "." . $random_longitude_decimal);
+    $random_location = ($random_latitude . "," . $random_longitude);
+    return $random_location;
+}
 
-echo $random_location
+function thing() {
+    $test = ("GET api.postcodes.io/postcodes?lon=" . uk_geo_generator($random_longitude) . "&lat=" . uk_geo_generator($random_latitude));
+    return $test;
+}
 
+//echo uk_geo_generator($random_location);
+echo uk_geo_generator();
+//thing();
 ?>
