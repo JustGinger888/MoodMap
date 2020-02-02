6addeadfc85f7ff2dfc9b2d204005bb1d1a45c09
Ethan Moffatt <ethanmoffatt888@icloud.com>
2020-02-02 18:35:43 +0000

    fnl

--- script.js
+++ script.js
@@ -59,9 +59,9 @@ document.getElementById('hap').addEventListener('click', function() {
         ['linear'],
         ['heatmap-density'],
         0, 'rgba(236,222,239,0)',
-        0.2, 'rgb(208,209,230)',
-        0.4, 'rgb(166,189,219)',
-        0.6, 'rgb(103,169,207)',
+        0.2, 'rgb(0, 153, 0)',
+        0.4, 'rgb(26, 255, 26)',
+        0.6, 'rgb(77, 255, 77)',
         0.8, 'rgb(28,144,153)'
       ],
       // increase radius as zoom increases
@@ -125,111 +125,9 @@ document.getElementById('hap').addEventListener('click', function() {
   }, 'waterway-label');
 });
 
+
 //Heatbox 
 document.getElementById('sad').addEventListener('click', function() {
-
-  map.addSource('tst2', {
-    type: 'geojson',
-    data: 'tst2.geojson'
-  });
-
-  // add heatmap layer here
-  map.addLayer({
-    id: 'tst2-heat',
-    type: 'heatmap',
-    source: 'tst2',
-    maxzoom: 15,
-    paint: {
-      // increase weight as diameter breast height increases
-      'heatmap-weight': {
-        property: 'dbh',
-        type: 'exponential',
-        stops: [
-          [1, 0],
-          [62, 1]
-        ]
-      },
-      // increase intensity as zoom level increases
-      'heatmap-intensity': {
-        stops: [
-          [3, 1],
-          [5, 1]
-        ]
-      },
-      // assign color values be applied to points depending on their density
-      'heatmap-color': [
-        'interpolate',
-        ['linear'],
-        ['heatmap-density'],
-        0, 'rgba(236,222,239,0)',
-        0.2, 'rgb(208,209,230)',
-        0.4, 'rgb(166,189,219)',
-        0.6, 'rgb(103,169,207)',
-        0.8, 'rgb(28,144,153)'
-      ],
-      // increase radius as zoom increases
-      'heatmap-radius': {
-        stops: [
-          [1, 4],
-          [3, 7]
-        ]
-      },
-      // decrease opacity to transition into the circle layer
-      'heatmap-opacity': {
-        default: 1,
-        stops: [
-          [14, 1],
-          [15, 0]
-        ]
-      },
-    }
-  }, 'waterway-label');
-  
-  // add circle layer here
-  map.addLayer({
-    id: 'tst2-point',
-    type: 'circle',
-    source: 'tst2',
-    minzoom: 14,
-    paint: {
-      // increase the radius of the circle as the zoom level and dbh value increases
-      'circle-radius': {
-        property: 'dbh',
-        type: 'exponential',
-        stops: [
-          [{ zoom: 15, value: 1 }, 5],
-          [{ zoom: 15, value: 62 }, 10],
-          [{ zoom: 22, value: 1 }, 20],
-          [{ zoom: 22, value: 62 }, 50],
-        ]
-      },
-      'circle-color': {
-        property: 'dbh',
-        type: 'exponential',
-        stops: [
-          [0, 'rgba(236,222,239,0)'],
-          [10, 'rgb(236,222,239)'],
-          [20, 'rgb(208,209,230)'],
-          [30, 'rgb(166,189,219)'],
-          [40, 'rgb(103,169,207)'],
-          [50, 'rgb(28,144,153)'],
-          [60, 'rgb(1,108,89)']
-        ]
-      },
-      'circle-stroke-color': 'white',
-      'circle-stroke-width': 1,
-      'circle-opacity': {
-        stops: [
-          [14, 0],
-          [15, 1]
-        ]
-      }
-    }
-  }, 'waterway-label');
-});
-
-//Heatbox 
-document.getElementById('ang').addEventListener('click', function() {
 
   map.addSource('tst3', {
     type: 'geojson',
@@ -293,6 +191,111 @@ document.getElementById('ang').addEventListener('click', function() {
     id: 'tst3-point',
     type: 'circle',
     source: 'tst3',
+    minzoom: 14,
+    paint: {
+      // increase the radius of the circle as the zoom level and dbh value increases
+      'circle-radius': {
+        property: 'dbh',
+        type: 'exponential',
+        stops: [
+          [{ zoom: 15, value: 1 }, 5],
+          [{ zoom: 15, value: 62 }, 10],
+          [{ zoom: 22, value: 1 }, 20],
+          [{ zoom: 22, value: 62 }, 50],
+        ]
+      },
+      'circle-color': {
+        property: 'dbh',
+        type: 'exponential',
+        stops: [
+          [0, 'rgba(236,222,239,0)'],
+          [10, 'rgb(236,222,239)'],
+          [20, 'rgb(208,209,230)'],
+          [30, 'rgb(166,189,219)'],
+          [40, 'rgb(103,169,207)'],
+          [50, 'rgb(28,144,153)'],
+          [60, 'rgb(1,108,89)']
+        ]
+      },
+      'circle-stroke-color': 'white',
+      'circle-stroke-width': 1,
+      'circle-opacity': {
+        stops: [
+          [14, 0],
+          [15, 1]
+        ]
+      }
+    }
+  }, 'waterway-label');
+});
+
+
+
+//Heatbox 
+document.getElementById('ang').addEventListener('click', function() {
+
+  map.addSource('tst2', {
+    type: 'geojson',
+    data: 'tst2.geojson'
+  });
+
+  // add heatmap layer here
+  map.addLayer({
+    id: 'tst2-heat',
+    type: 'heatmap',
+    source: 'tst2',
+    maxzoom: 15,
+    paint: {
+      // increase weight as diameter breast height increases
+      'heatmap-weight': {
+        property: 'dbh',
+        type: 'exponential',
+        stops: [
+          [1, 0],
+          [62, 1]
+        ]
+      },
+      // increase intensity as zoom level increases
+      'heatmap-intensity': {
+        stops: [
+          [3, 1],
+          [5, 1]
+        ]
+      },
+      // assign color values be applied to points depending on their density
+      'heatmap-color': [
+        'interpolate',
+        ['linear'],
+        ['heatmap-density'],
+        0, 'rgba(236,222,239,0)',
+        0.2, 'rgb(230, 0, 0)',
+        0.4, 'rgb(255, 51, 51)',
+        0.6, 'rgb(223, 47, 47)',
+        0.8, 'rgb(179, 0, 0)',
+      ],
+      // increase radius as zoom increases
+      'heatmap-radius': {
+        stops: [
+          [1, 4],
+          [3, 7]
+        ]
+      },
+      // decrease opacity to transition into the circle layer
+      'heatmap-opacity': {
+        default: 1,
+        stops: [
+          [14, 1],
+          [15, 0]
+        ]
+      },
+    }
+  }, 'waterway-label');
+  
+  // add circle layer here
+  map.addLayer({
+    id: 'tst2-point',
+    type: 'circle',
+    source: 'tst2',
     minzoom: 14,
     paint: {
       // increase the radius of the circle as the zoom level and dbh value increases
